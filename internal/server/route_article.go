1a0dac28d111de4600beacb41ae9d7dd7190128f
dickyarya404 <dickyarya404@gmail.com>
2024-05-29 21:52:13 +0700

    feat article route

--- internal/server/route_article.go
+++ internal/server/route_article.go
@@ -2,21 +2,29 @@ package server
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/sawalreverr/recything/internal/feature/article/handler"
+	"github.com/sawalreverr/recything/internal/feature/article/repository"
+	"github.com/sawalreverr/recything/internal/feature/article/service"
 	"gorm.io/gorm"
 )
 
 func RouteArticle(e *echo.Group, db *gorm.DB) {
 
-	//manage article
-	// trashRepo := trashRepository.NewTrashCategoryRepository(db)
-	// articleRepo := repository.NewArticleRepository(db,trashRepo)
-	// articleServ := service.NewArticleService(articleRepo)
-	// articleHand := handler.NewArticleHandler(articleServ)
+	// manage article
+	trashRepo := trashRepository.NewTrashCategoryRepository(db)
+	articleRepo := repository.NewArticleRepository(db, trashRepo)
+	articleServ := service.NewArticleService(articleRepo)
+	articleHand := handler.NewArticleHandler(articleServ)
 
-	// admin := e.Group("/admins/manage/articles", jwt.JWTMiddleware())
-	// admin.POST("", articleHand.CreateArticle)
-	// admin.GET("", articleHand.GetAllArticle)
-	// admin.GET("/:id", articleHand.GetSpecificArticle)
-	// admin.PUT("/:id", articleHand.UpdateArticle)
-	// admin.DELETE("/:id", articleHand.DeleteArticle)
+	admin := e.Group("/admins/manage/articles", jwt.JWTMiddleware())
+	admin.POST("", articleHand.CreateArticle)
+	admin.GET("", articleHand.GetAllArticle)
+	admin.GET("/:id", articleHand.GetSpecificArticle)
+	admin.PUT("/:id", articleHand.UpdateArticle)
+	admin.DELETE("/:id", articleHand.DeleteArticle)
+
+	user := e.Group("/articles", jwt.JWTMiddleware())
+	user.GET("", articleHand.GetAllArticleUser)
+	user.GET("/:id", articleHand.GetSpecificArticle)
+	user.GET("/popular", articleHand.GetPopularArticle)
 }
