bae6714b1f248ae4058bf74304af93231e490e44
markus <markusrr10@gmail.com>
2024-06-01 09:18:36 +0700

    create entity for user task

---
+++ internal/task/user_task/entity/user_task_entity.go
@@ -0,0 +1,32 @@
+package entity
+
+import (
+	"time"
+
+	task "github.com/sawalreverr/recything/internal/task/manage_task/entity"
+	"github.com/sawalreverr/recything/internal/user"
+	"gorm.io/gorm"
+)
+
+type UserTaskChallenge struct {
+	ID              string             `gorm:"primaryKey"`
+	UserId          string             `gorm:"index"`
+	User            user.User          `gorm:"foreignKey:UserId"`
+	TaskChallengeId string             `gorm:"index"`
+	TaskChallenge   task.TaskChallenge `gorm:"foreignKey:TaskChallengeId"`
+	Status          bool
+	CreatedAt       time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt       time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt       gorm.DeletedAt `gorm:"index"`
+}
+
+type UserTaskImage struct {
+	ID              string             `gorm:"primaryKey"`
+	TaskChallengeId string             `gorm:"index"`
+	TaskChallenge   task.TaskChallenge `gorm:"foreignKey:TaskChallengeId"`
+	ImageUrl        string
+	Description     string
+	CreatedAt       time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt       time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt       gorm.DeletedAt `gorm:"index"`
+}
