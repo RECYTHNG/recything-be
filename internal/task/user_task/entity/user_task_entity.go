2265074f3f25d8f58e54186454a2e831542db536
markus <markusrr10@gmail.com>
2024-06-01 15:34:55 +0700

    udpate entity user task challenge

--- internal/task/user_task/entity/user_task_entity.go
+++ internal/task/user_task/entity/user_task_entity.go
@@ -9,23 +9,24 @@ import (
 )
 
 type UserTaskChallenge struct {
-	ID              string             `gorm:"primaryKey"`
-	UserId          string             `gorm:"index"`
-	User            user.User          `gorm:"foreignKey:UserId"`
-	TaskChallengeId string             `gorm:"index"`
-	TaskChallenge   task.TaskChallenge `gorm:"foreignKey:TaskChallengeId"`
-	Status          bool
-	ImageTask       []UserTaskImage `gorm:"foreignKey:TaskChallengeId"`
-	CreatedAt       time.Time       `gorm:"autoCreateTime"`
-	UpdatedAt       time.Time       `gorm:"autoUpdateTime"`
-	DeletedAt       gorm.DeletedAt  `gorm:"index"`
+	ID               string             `gorm:"primaryKey"`
+	UserId           string             `gorm:"index"`
+	User             user.User          `gorm:"foreignKey:UserId"`
+	TaskChallengeId  string             `gorm:"index"`
+	TaskChallenge    task.TaskChallenge `gorm:"foreignKey:TaskChallengeId"`
+	StatusProgress   string             `gorm:"type:enum('in_progress', 'done');default:'in_progress'"`
+	StatusAccept     string             `gorm:"type:enum('accept','need_rivew', 'reject');default:'need_rivew'"`
+	ImageTask        []UserTaskImage    `gorm:"foreignKey:TaskChallengeId"`
+	DescriptionImage string
+	CreatedAt        time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt        time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt        gorm.DeletedAt `gorm:"index"`
 }
 
 type UserTaskImage struct {
 	ID              string `gorm:"primaryKey"`
 	TaskChallengeId string `gorm:"index"`
 	ImageUrl        string
-	Description     string
 	CreatedAt       time.Time      `gorm:"autoCreateTime"`
 	UpdatedAt       time.Time      `gorm:"autoUpdateTime"`
 	DeletedAt       gorm.DeletedAt `gorm:"index"`
