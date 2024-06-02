d9f0e904b5373315b427b9c930ad8f72c57e40ee
markus <markusrr10@gmail.com>
2024-06-02 22:12:45 +0700

    feat(task): approval task, create entity for approval task

--- internal/task/approval_task/entity/approval_task_entity.go
+++ internal/task/approval_task/entity/approval_task_entity.go
@@ -1,1 +1,19 @@
 package entity
+
+import (
+	"time"
+
+	user_task "github.com/sawalreverr/recything/internal/task/user_task/entity"
+	"gorm.io/gorm"
+)
+
+type ApprovalTask struct {
+	ID                  string `gorm:"primaryKey"`
+	UserTaskChallengeId string `gorm:"index"`
+	UserTaskChallenge   user_task.UserTaskChallenge
+
+	Point     int
+	CreatedAt time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
