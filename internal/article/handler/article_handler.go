5df52cf777dc55f62b33e5e10aea39097603e90d
dickyarya404 <dickyarya404@gmail.com>
2024-06-04 22:30:30 +0700

    feat article

--- internal/article/handler/article_handler.go
+++ internal/article/handler/article_handler.go
@@ -1,11 +1,12 @@
 package handler
 
-import "github.com/labstack/echo/v4"
+import "github.com/sawalreverr/recything/internal/article/entity"
 
-type ArticleHandler interface {
-	CreateArticleHandler(echo.Context) error
-	GetDataAllArticleHandler(echo.Context) error
-	GetDataArticleByIdHandler(echo.Context) error
-	UpdateArticleHandler(echo.Context) error
-	DeleteArticleHandler(echo.Context) error
+type ArticleUsecase interface {
+	UploadArticle(article entity.Article) (*entity.Article, error)
+	UpdateArticle(articleID string, article entity.Article) error
+	DeleteArticle(articleID string) error
+
+	FindArticleByID(articleID string) (*entity.Article, error)
+	FindAllArticles(page int, limit int, sortBy string, sortType string) (*[]entity.Article, error)
 }
