a58cb831614547c36790805e03f824afe1ca9499
Mark <markusrr10@gmail.com>
2024-06-04 13:05:33 +0700

    add entity archievement

--- internal/archievements/manage_archievements/entity/manage_archievement_entity.go
+++ internal/archievements/manage_archievements/entity/manage_archievement_entity.go
@@ -1,1 +1,17 @@
 package entity
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Archievement struct {
+	ID          int `json:"id" gorm:"primaryKey"`
+	Level       string
+	TargetPoint int
+	BadgeUrl    string
+	CreatedAt   time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt   time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt   gorm.DeletedAt `gorm:"index"`
+}
