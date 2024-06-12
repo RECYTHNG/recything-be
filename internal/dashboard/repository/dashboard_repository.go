96d59aab89754b81337a755f3b7d42f3e2ead777
Mark <markusrr10@gmail.com>
2024-06-12 22:58:17 +0700

    add some repository interface for dashboard

--- internal/dashboard/repository/dashboard_repository.go
+++ internal/dashboard/repository/dashboard_repository.go
@@ -1,1 +1,14 @@
 package repository
+
+type DashboardRepository interface {
+	GetTotalUser() (int, error)
+	GetTotalReport() (int, error)
+	GetTotalChange() (int, error)
+	GetTotalContent() (int, error)
+	GetUserClassic() (int, error)
+	GetUserSilver() (int, error)
+	GetUserGold() (int, error)
+	GetUserPlatinum() (int, error)
+	GetReportLittering() (int, error)
+	GetReportRubbish() (int, error)
+}
